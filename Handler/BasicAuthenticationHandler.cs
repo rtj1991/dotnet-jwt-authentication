2c7c3d8902225b4c7ba552507a32465155fcc032
thara <rtj1991@gmail.com>
2022-07-27 18:00:53 +0530

    authentication post

--- Handler/BasicAuthenticationHandler.cs
+++ Handler/BasicAuthenticationHandler.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 using System.Text.Encodings.Web;
 using System.Net.Http.Headers;
+using System.Text;
 using ProductAPI.Models;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace ProductAPI.Handler
@@ -20,10 +23,38 @@ namespace ProductAPI.Handler
 
         protected async override Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            if(!Request.Headers.ContainsKey("Authorization"))
-            return AuthenticateResult.Fail("No Header Found");
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("No Header Found");
 
-var _headervalue=AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+            var _headervalue = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+            Console.WriteLine("------> " + _headervalue);
+            var bytes = Convert.FromBase64String(_headervalue.Parameter);
+            string credentials = Encoding.UTF8.GetString(bytes);
+
+            if (!string.IsNullOrEmpty(credentials))
+            {
+
+                string[] array = credentials.Split(":");
+                string username = array[0];
+                string password = array[1];
+                var user = this._DBContext.Users.FirstOrDefault(item => item.Email == username && item.Password == password);
+
+                if (user == null)
+                    return AuthenticateResult.Fail("Unauthorized");
+
+                var claim = new[] { new Claim(ClaimTypes.Name, username) };
+                var identity = new ClaimsIdentity(claim, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                Console.WriteLine("ticket======> " + ticket);
+                return AuthenticateResult.Success(ticket);
+
+            }
+            else
+            {
+                return AuthenticateResult.Fail("UnAuthorized");
+            }
 
         }
 
